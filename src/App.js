74874199ac24f749b7884b8f5229c69873957b3a
Dariusz Okoński <darek200180@gmail.com>
2026-01-21 06:30:30 +0100

    section 8

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import './App.css';
 import RandomNumber from './excerciseComponents/section_06/RandomNumber/RandomNumber';
 
 function App() {
-  return (
-    <div className='App'>
-      <RandomNumber />
-    </div>
-  );
+  return <div className='App'>section 8</div>;
 }
 
 export default App;
