175c031e95e5bbacd69aca76fbde9351ca0b34c1
Dariusz Okoński <darek200180@gmail.com>
2026-01-21 06:44:16 +0100

    add ScoreKeeper

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import RandomNumber from './excerciseComponents/section_06/RandomNumber/RandomNumber';
+import ScoreKeeper from './excerciseComponents/section_08/ScoreKeeper/ScoreKeeper';
 
 function App() {
-  return <div className='App'>section 8</div>;
+  return (
+    <div className='App'>
+      <ScoreKeeper />
+    </div>
+  );
 }
 
 export default App;
