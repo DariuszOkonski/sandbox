dd7fe307d5a90654b4596fb8d4dd37c0c14ab339
Dariusz Okoński <darek200180@gmail.com>
2026-02-04 07:35:53 +0100

    add first form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Form_1 from './excerciseComponents/section_13/Form_1/Form_1';
 
 function App() {
   return (
     <div className='App'>
       <h1>Section 13</h1>
+
+      <Form_1 />
     </div>
   );
 }
---
+++ src/excerciseComponents/section_13/Form_1/Form_1.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Form_1 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { username: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({ username: evt.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Form Demo</h1>
+
+        <form>
+          <input
+            type='text'
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form_1;
