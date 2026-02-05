f9069451fe1f1378b9031d6f1e6d1b7dc83234c3
Dariusz Okoński <darek200180@gmail.com>
2026-02-05 07:27:36 +0100

    update MultipleForm

--- src/excerciseComponents/section_13/MultipleForm/MultipleForm.js
+++ src/excerciseComponents/section_13/MultipleForm/MultipleForm.js
@@ -4,21 +4,29 @@ class MultipleForm extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { username: '' };
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(evt) {
-    this.setState({ username: evt.target.value });
+    this.setState({ [evt.target.name]: evt.target.value });
   }
 
   handleSubmit(evt) {
     evt.preventDefault();
 
     console.log('you submitted');
-    this.setState({ username: '' });
+    this.setState({
+      username: '',
+      email: '',
+      password: '',
+    });
   }
 
   render() {
@@ -30,7 +38,24 @@ class MultipleForm extends Component {
             type='text'
             value={this.state.username}
             onChange={this.handleChange}
+            placeholder='username'
+            name='username'
           />
+          <input
+            type='email'
+            placeholder='email'
+            value={this.state.email}
+            onChange={this.handleChange}
+            name='email'
+          />
+          <input
+            type='password'
+            placeholder='password'
+            value={this.state.password}
+            onChange={this.handleChange}
+            name='password'
+          />
+
           <button>Submit</button>
         </form>
       </div>
