c219d3368985bab0c02147f66e7d07bb0d85fb71
Dariusz Okoński <darek200180@gmail.com>
2026-02-10 07:34:21 +0100

    add Box component

--- src/excerciseComponents/section_14/ColorBoxMaker/AppColorBoxMaker.js
+++ src/excerciseComponents/section_14/ColorBoxMaker/AppColorBoxMaker.js
@@ -1,10 +1,45 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
+import Box from './Box';
 
 class AppColorBoxMaker extends Component {
   render() {
     return (
       <div>
         <h1>Color BoxMaker</h1>
+
+        <div
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Box height={10} width={10} color='red' />
+          <Box height={10} width={10} color='green' />
+          <Box height={10} width={10} color='blue' />
+          <Box height={10} width={10} color='red' />
+          <Box height={10} width={10} color='green' />
+          <Box height={10} width={10} color='blue' />
+          <Box height={10} width={10} color='red' />
+          <Box height={10} width={10} color='green' />
+          <Box height={10} width={10} color='blue' />
+          <Box height={10} width={10} color='red' />
+          <Box height={10} width={10} color='green' />
+          <Box height={10} width={10} color='blue' />
+          <Box height={10} width={10} color='red' />
+          <Box height={10} width={10} color='green' />
+          <Box height={10} width={10} color='blue' />
+          <Box height={10} width={10} color='red' />
+          <Box height={10} width={10} color='green' />
+          <Box height={10} width={10} color='blue' />
+          <Box height={10} width={10} color='red' />
+          <Box height={10} width={10} color='green' />
+          <Box height={10} width={10} color='blue' />
+          <Box height={10} width={10} color='red' />
+          <Box height={10} width={10} color='green' />
+          <Box height={10} width={10} color='blue' />
+        </div>
       </div>
     );
   }
