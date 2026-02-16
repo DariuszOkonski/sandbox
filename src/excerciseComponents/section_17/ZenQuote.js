4bff9a5ff32061136c095d9d02252121bc8ba6e1
Dariusz Okoński <darek200180@gmail.com>
2026-02-16 07:34:52 +0100

    use axios

--- src/excerciseComponents/section_17/ZenQuote.js
+++ src/excerciseComponents/section_17/ZenQuote.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react';
+import axios from 'axios';
+import { Component } from 'react';
 
 class ZenQuote extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quote: null,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/zen')
+      .then((response) => {
+        this.setState({
+          quote: response.data,
+        });
+      })
+      .catch((err) => {
+        console.log('err: ', err);
+      });
+  }
+
   render() {
     return (
       <div>
-        <h1>ZenQuote</h1>
+        <h1>Always remember...</h1>
+        <p>{this.state.quote ? this.state.quote : 'Loading...'}</p>
       </div>
     );
   }
