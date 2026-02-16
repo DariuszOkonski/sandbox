1a619f6b0761b9684d0eb27dd992709abee6da31
Dariusz Okoński <darek200180@gmail.com>
2026-02-16 07:36:49 +0100

    remove constructor from App

--- src/excerciseComponents/section_17/AppLifeCycle.js
+++ src/excerciseComponents/section_17/AppLifeCycle.js
@@ -2,10 +2,6 @@ import { Component } from 'react';
 import ZenQuote from './ZenQuote';
 
 class AppLifeCycle extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div>
