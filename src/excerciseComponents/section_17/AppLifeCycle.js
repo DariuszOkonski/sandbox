621ff5d396e0d8c9213a1ef66bfa629d8bba4571
Dariusz Okoński <darek200180@gmail.com>
2026-02-16 07:27:41 +0100

    add ZenQuote

--- src/excerciseComponents/section_17/AppLifeCycle.js
+++ src/excerciseComponents/section_17/AppLifeCycle.js
@@ -1,29 +1,15 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
+import ZenQuote from './ZenQuote';
 
 class AppLifeCycle extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      time: new Date(),
-    };
-    console.log('In Constructor');
-  }
-
-  componentDidMount() {
-    console.log('In ComponentDidMount');
-
-    this.timerID = setInterval(() => {
-      this.setState({ time: new Date() });
-    }, 1000);
   }
 
   render() {
-    console.log('In Render');
-
     return (
       <div>
-        <h1>{this.state.time.getSeconds()}</h1>
+        <ZenQuote />
       </div>
     );
   }
---
+++ src/excerciseComponents/section_17/ZenQuote.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ZenQuote extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ZenQuote</h1>
+      </div>
+    );
+  }
+}
+
+export default ZenQuote;
