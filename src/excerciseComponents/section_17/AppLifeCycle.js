710bde968efa8deb028304e6864e5cdf8e92256b
Dariusz Okoński <darek200180@gmail.com>
2026-02-16 07:23:24 +0100

    use componentDidMount

--- src/excerciseComponents/section_17/AppLifeCycle.js
+++ src/excerciseComponents/section_17/AppLifeCycle.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class AppLifeCycle extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      time: new Date(),
+    };
+    console.log('In Constructor');
+  }
+
+  componentDidMount() {
+    console.log('In ComponentDidMount');
+
+    this.timerID = setInterval(() => {
+      this.setState({ time: new Date() });
+    }, 1000);
+  }
+
   render() {
+    console.log('In Render');
+
     return (
       <div>
-        <h1>LifeCycle</h1>
+        <h1>{this.state.time.getSeconds()}</h1>
       </div>
     );
   }
