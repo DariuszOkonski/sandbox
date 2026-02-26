249124481573b47f4f84e3fa514ec718cea10ca2
Dariusz Okoński <darek200180@gmail.com>
2026-02-26 06:56:06 +0100

    add getEmoji class

--- src/excerciseComponents/section_19/Joke.js
+++ src/excerciseComponents/section_19/Joke.js
@@ -20,18 +20,38 @@ class Joke extends Component {
     }
   }
 
+  getEmoji() {
+    if (this.props.votes >= 15) {
+      return 'em em-rolling_on_the_floor_laughing';
+    } else if (this.props.votes >= 12) {
+      return 'em em-laughing';
+    } else if (this.props.votes >= 9) {
+      return 'em em-smiley';
+    } else if (this.props.votes >= 6) {
+      return 'em em-slightly_smiling_face';
+    } else if (this.props.votes >= 3) {
+      return 'em em-neutral_face';
+    } else if (this.props.votes >= 0) {
+      return 'em em-confused';
+    } else {
+      return 'em em-angry';
+    }
+  }
+
   render() {
     return (
       <div className='Joke'>
         <div className='Joke-buttons'>
           <i className='fas fa-arrow-up' onClick={this.props.upvote}></i>
-          <span className='Joke-votes'>{this.props.votes}</span>
+          <span className='Joke-votes' style={{ borderColor: this.getColor() }}>
+            {this.props.votes}
+          </span>
           <i className='fas fa-arrow-down' onClick={this.props.downvote}></i>
         </div>
 
         <div className='Joke-text'>{this.props.text}</div>
         <div className='Joke-smiley'>
-          <i className='em em-rolling_on_the_floor_laughing' />
+          <i className={this.getEmoji()} />
         </div>
       </div>
     );
