d79f14eddb3811acf446ffcf76e460bb11a034e0
Dariusz Okoński <darek200180@gmail.com>
2026-03-03 06:42:53 +0100

    add Hater component

--- src/excerciseComponents/section_20/AppRouter.js
+++ src/excerciseComponents/section_20/AppRouter.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dog from './Dog';
+import About from './About';
+import Contact from './Contact';
+import Hater from './Hater';
 
 class AppRouter extends Component {
   render() {
     return (
       <BrowserRouter>
         <Routes>
-          <Route exact path='/' element={<h1>Home</h1>} />
+          <Route exact path='/' element={<About />} />
           <Route path='/dog' element={<Dog />} />
+          <Route path='/hater' element={<Hater />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
       </BrowserRouter>
     );
---
+++ src/excerciseComponents/section_20/Hater.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Hater extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I absolutely hate dogs</h1>
+      </div>
+    );
+  }
+}
+
+export default Hater;
