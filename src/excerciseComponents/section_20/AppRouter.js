c72e56d7b685968f4904d9b1234fc09301565a72
Dariusz Okoński <darek200180@gmail.com>
2026-03-04 07:15:18 +0100

    add routes

--- src/excerciseComponents/section_20/AppRouter.js
+++ src/excerciseComponents/section_20/AppRouter.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import Dog from './Dog';
 import About from './About';
 import Contact from './Contact';
 import Hater from './Hater';
+import './AppRouter.css';
 
 class AppRouter extends Component {
   render() {
     return (
       <BrowserRouter>
         <nav>
-          <Link to='/'>About</Link>
-          <Link to='/dog'>Dog</Link>
-          <Link to='/dog/hater'>Hater</Link>
-          <Link to='/contact'>Contact</Link>
+          <NavLink to='/'>About</NavLink>
+          <NavLink end to='/dog'>
+            Dog
+          </NavLink>
+          <NavLink to='/dog/hater'>Hater</NavLink>
+          <NavLink to='/contact'>Contact</NavLink>
         </nav>
         <Routes>
-          <Route exact path='/' element={<About />} />
+          <Route path='/' element={<About />} />
           <Route path='/dog' element={<Dog />} />
           <Route path='/dog/hater' element={<Hater />} />
           <Route path='/contact' element={<Contact />} />
