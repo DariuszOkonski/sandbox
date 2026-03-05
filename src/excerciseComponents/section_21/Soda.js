7867c482e55ebc9f71360285d32f8069761c3ff6
Dariusz Okoński <darek200180@gmail.com>
2026-03-05 06:26:15 +0100

    use Link component

--- src/excerciseComponents/section_21/Soda.js
+++ src/excerciseComponents/section_21/Soda.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Soda extends Component {
   render() {
     return (
       <div>
         <h1>Soda</h1>
+
+        <Link to='/'>Go Back</Link>
       </div>
     );
   }
--- src/excerciseComponents/section_21/VendingMachine.js
+++ src/excerciseComponents/section_21/VendingMachine.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class VendingMachine extends Component {
   render() {
     return (
       <div>
         <h1>VendingMachine</h1>
+        <Link to='/soda'>Soda</Link>
+        <Link to='/chips'>Chips</Link>
+        <Link to='/sardines'>Sardines</Link>
       </div>
     );
   }
