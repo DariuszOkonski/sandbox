7cafc1a72e287c40c9eec228636bac2dbd3d807a
Dariusz Okoński <darek200180@gmail.com>
2026-03-05 06:21:02 +0100

    add routes

--- src/excerciseComponents/section_21/AppSection21.js
+++ src/excerciseComponents/section_21/AppSection21.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import VendingMachine from './VendingMachine';
+import Chips from './Chips';
+import Sardines from './Sardines';
+import Soda from './Soda';
 
 class AppSection21 extends Component {
   render() {
-    return <div></div>;
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<VendingMachine />} />
+          <Route path='/soda' element={<Soda />} />
+          <Route path='/sardines' element={<Sardines />} />
+          <Route path='/chips' element={<Chips />} />
+        </Routes>
+      </BrowserRouter>
+    );
   }
 }
 
