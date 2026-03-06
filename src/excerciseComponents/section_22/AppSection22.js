3d1944229a35868783bcd93781a0e89b4826f9cc
Dariusz Okoński <darek200180@gmail.com>
2026-03-06 06:13:24 +0100

    add Food component

--- src/excerciseComponents/section_22/AppSection22.js
+++ src/excerciseComponents/section_22/AppSection22.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Food from './Food';
 
 class AppSection22 extends Component {
   render() {
     return (
       <div>
         <h1>Section 22</h1>
+        <Food name='Kale' />
       </div>
     );
   }
